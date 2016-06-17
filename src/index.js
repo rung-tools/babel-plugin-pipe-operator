d3cdd7f074dc0903a5b169859fe2d2b0006ab594
Swizz <gerodel.quentin@gmail.com>
2016-06-17 19:07:52 +0200

    Updates core

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export default () => {
+export default ({types: t}) => {
   const hasDirective = (path, directive) => {
     !!path.findParent(({ node }) => {
       node.directives && node.directives.some(({ value }) => {
@@ -13,12 +13,9 @@ export default () => {
         if (!path.isBinaryExpression({ operator: "|" })) return
         if (hasDirective(path, "no pipe")) return
 
-        const rightPath = path.get("right")
+        let args = t.isSequenceExpression(path.node.left) ? path.node.left.expressions : [path.node.left];
 
-        rightPath.assertCallExpression()
-
-        rightPath.node.arguments.unshift(path.node.left)
-        path.replaceWith(rightPath)
+        path.replaceWith(t.callExpression(path.node.right, args))
 
         return
       }
